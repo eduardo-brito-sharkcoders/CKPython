6d58321ae04129fa01da77d4d24e92f0cbdf7856
eduardobrito000 <eduardo.brito.t0126041@edu.atec.pt>
2024-02-06 12:32:25 +0000

    update

---
+++ print_input.py
@@ -0,0 +1,4 @@
+nome_pessoa = input("Qual é o seu nome: ")
+idade_pessoa = input("Qual é a sua idade: ")
+
+print("Olá " + nome_pessoa + ", você tem " + idade_pessoa + " anos de idade.")
---
+++ variaveis.py
@@ -0,0 +1,14 @@
+'''
+
+Como criar uma variável:
+
+    <nome_da_variavel> = <valor>
+
+Tipos de variáveis:
+
+    variavel_int = 1                -> inteiro  int()
+    variavel_float = 1.0            -> decimal  float()
+    variavel_str = "Texto"          -> texto    str()
+    variavel_bool = True ou False   -> booleano bool()
+
+'''
